a4edac3ae1fd847476c1a962c72c83d2dd3c64f2
John Croft <john.croft@finra.org>
2023-12-22 16:24:21 +0000

    Clean up.

--- nix/flakes/home.nix
+++ nix/flakes/home.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }:
 
-
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "jcroft";
   home.homeDirectory = "/home/jcroft";
-
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -17,20 +15,19 @@
   # release notes.
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
-
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     hello
-    cowsay
 
+    iputils
 
     btop
     bat
     fzf
-
+    rsync
 
     git
     gitui
@@ -38,16 +35,14 @@
     gcc
     neovim
 
-
     podman
-
+    podman-compose
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     (pkgs.nerdfonts.override { fonts = [ "IBMPlexMono" ]; })
-
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
@@ -57,7 +52,6 @@
     # '')
   ];
 
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
@@ -66,14 +60,14 @@
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
 
-
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-  };
 
+    "bin/docker".source = config.lib.file.mkOutOfStoreSymlink "/home/jcroft/.nix-profile/bin/podman";
+  };
 
   # You can also manage environment variables but you will have to manually
   # source
@@ -86,9 +80,10 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
+  home.sessionPath = [ "/home/jcroft/bin" ];
 
   # Let Home Manager install and manage itself.
   programs = {
@@ -96,20 +91,22 @@
     bash = {
       enable = true;
       shellAliases = {
+        cls = "clear";
         vi = "nvim";
         vim = "nvim";
       };
+      initExtra = ''
+        . "/home/jcroft/.nix-profile/etc/profile.d/hm-session-vars.sh"
+      '';
     };
     git = {
       enable = true;
-      userName = [Proper name here];
-      userEmail = [email address here];
+      userName = "John Croft";
+      userEmail = "john.croft@finra.org";
       extraConfig = {
         pull.rebase = false;
       };
     };
   };
 }
-
-
 
