54ecd081a6cc7fc5b023cc82128bfbce90a23376
John Croft <john.croft@finra.org>
2023-12-27 22:37:41 +0000

    Starting home-manager config with flakes.

---
+++ nix/nixpkgs/home.nix
@@ -0,0 +1,7 @@
+{ pkgs, ... }: {
+  home.username = "jcroft"; # REPLACE ME
+  home.homeDirectory = "/home/jcoft"; # REPLACE ME
+  home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
+}
+
