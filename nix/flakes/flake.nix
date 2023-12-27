4c6f7e1dde0fb6944db7178911f50cd37e83d8f0
John Croft <john.croft@finra.org>
2023-12-27 21:09:10 +0000

    Updated flake to run hello.

--- nix/flakes/flake.nix
+++ nix/flakes/flake.nix
@@ -1,19 +1,31 @@
 {
   description = "A Basic Flake (Template)";
-
+  
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem(system: 
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        packages = {
-          hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-          default = self.packages.x86_64-linux.hello;
+  outputs = { self, nixpkgs, flake-utils }:
+    # calling a function from `flake-utils` that takes a lambda
+    # that takes the system we're targetting
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # no need to define `system` anymore
+        name = "hello";
+        src = ./.;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        # `eachDefaultSystem` transforms the input, our output set
+        # now simply has `packages.default` which gets turned into
+        # `packages.${system}.default` (for each system)
+        packages.default = derivation {
+          inherit system name src;
+          builder = with pkgs; "${bash}/bin/bash";
+          args = [ "-c" "echo hello > $out" ];
         };
       }
     );
 }
+
