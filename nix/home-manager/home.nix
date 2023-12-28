633e26555ce0bd20a2e99899c6e0eaaab41b2537
John Croft <jcroft@coderz.io>
2023-12-28 01:56:41 +0000

    Added packages into flake config.

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -3,5 +3,40 @@
   home.homeDirectory = "/home/jcroft";
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    hello
+
+    iputils
+
+    btop
+    bat
+    fzf
+    rsync
+
+    git
+    gitui
+    tmux
+    gcc
+    neovim
+
+    podman
+    podman-compose
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    (pkgs.nerdfonts.override { fonts = [ "IBMPlexMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
 }
 
