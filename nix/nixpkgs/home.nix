acbfb5996f38bb805acac7b00374284a55c4c491
John Croft <jcroft@coderz.io>
2023-12-28 01:22:38 +0000

    Fixed typo in username.

--- nix/nixpkgs/home.nix
+++ nix/nixpkgs/home.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   home.username = "jcroft"; 
-  home.homeDirectory = "/home/jcoft";
+  home.homeDirectory = "/home/jcroft";
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
 }
