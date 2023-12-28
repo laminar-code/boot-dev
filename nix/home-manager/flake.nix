7638b56dd69282bb148f7a53e9ad6c4c715c8bd5
John Croft <jcroft@coderz.io>
2023-12-28 15:27:58 +0000

    Implemented flake-utils in home-manager flake.

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -1,25 +1,57 @@
 {
-  description = "Home Manager configuration";
-
+  description = "Home Manager Flake";
+  
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: let
-    arch = "aarch64-linux";
-  in {
-    defaultPackage.${arch} =
-      home-manager.defaultPackage.${arch};
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }:
+    # calling a function from `flake-utils` that takes a lambda
+    # that takes the system we're targetting
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # no need to define `system` anymore
+        name = "home-manager";
+        src = ./.;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        # `eachDefaultSystem` transforms the input, our output set
+        # now simply has `packages.default` which gets turned into
+        # `packages.${system}.default` (for each system)
+        packages.default = derivation {
+          inherit system name src;
+          builder = with pkgs; "${bash}/bin/bash";
+          args = [ "-c" "echo hello > $out" ];
+        };
 
-    homeConfigurations.jcroft = 
-      home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${arch};
-        modules = [ ./home.nix ];
-      };
-    };
+        homeConfigurations.jcroft = 
+          home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home.nix ];
+          };
+      }
+    );
 }
+
+
+#{
+#  outputs = { nixpkgs, home-manager, ... }: let
+#    arch = "aarch64-linux";
+#  in {
+#    defaultPackage.${arch} =
+#      home-manager.defaultPackage.${arch};
+#
+#    homeConfigurations.jcroft = 
+#      home-manager.lib.homeManagerConfiguration {
+#        pkgs = nixpkgs.legacyPackages.${arch};
+#        modules = [ ./home.nix ];
+#      };
+#    };
+#}
 
