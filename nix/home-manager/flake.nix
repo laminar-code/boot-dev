7e2c67cf86617a6a7d1be7ad442483998b71146e
John Croft <jcroft@coderz.io>
2025-04-02 16:36:59 -0400

    Updated nix and home-manager packages versions.

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -3,9 +3,9 @@
   description = "Home Manager Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
