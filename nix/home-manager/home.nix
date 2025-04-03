3bb9ef77ecea627e17bfb59de3eae9b1f726ebf5
John Croft <jcroft@coderz.io>
2025-04-03 15:06:35 -0400

    Fixed typo in font configuration.

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -83,7 +83,7 @@
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-    pkgs.nerdfonts.blex-mono
+    pkgs.nerd-fonts.blex-mono
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
