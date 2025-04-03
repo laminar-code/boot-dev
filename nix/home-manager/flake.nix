f05080999fb1c8ee3a594743a3978fac8f1dcfae
John Croft <jcroft@coderz.io>
2025-04-03 09:26:15 -0400

    Updated nixpkgs to 24.11.

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -3,7 +3,7 @@
   description = "Home Manager Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
