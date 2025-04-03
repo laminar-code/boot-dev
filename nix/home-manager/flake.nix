6b592c95e86e79beead9f533fe4c473dc79fcff6
John Croft <jcroft@coderz.io>
2025-04-03 09:57:54 -0400

    Moved nixpkgs to unstable to avoid neovim bug.

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -3,7 +3,7 @@
   description = "Home Manager Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
