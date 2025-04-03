e236ac833707346a7d653522585f0ebee69b320e
John Croft <jcroft@coderz.io>
2025-04-03 14:00:16 -0400

    Updating home-manager flake to remove defaultPackage reference.

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -15,8 +15,8 @@
     user = "devx";
     pkgs = import nixpkgs { inherit system; };
   in {
-    defaultPackage.${system} =
-      home-manager.defaultPackage.${system};
+    # defaultPackage.${system} =
+    #   home-manager.defaultPackage.${system};
 
     homeConfigurations = {
       ${user} = home-manager.lib.homeManagerConfiguration {
@@ -25,5 +25,29 @@
       };
     };
   };
-}
 
+  # outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  #   nixosConfigurations = {
+  #     # TODO please change the hostname to your own
+  #     my-nixos = nixpkgs.lib.nixosSystem {
+  #       system = "x86_64-linux";
+  #       modules = [
+  #         ./configuration.nix
+
+  #         # make home-manager as a module of nixos
+  #         # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+  #         home-manager.nixosModules.home-manager
+  #         {
+  #           home-manager.useGlobalPkgs = true;
+  #           home-manager.useUserPackages = true;
+
+  #           # TODO replace ryan with your own username
+  #           home-manager.users.ryan = import ./home.nix;
+
+  #           # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+  #         }
+  #       ];
+  #     };
+  #   };
+  # };
+}
